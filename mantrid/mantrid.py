0a1b72cd0968e0f4c9895327326f7fce6d88b3b1
szibis <szibis@gmail.com>
2013-12-15 01:10:18 +0100

    Diamond Mantrid Collector first release

---
+++ mantrid/mantrid.py
@@ -0,0 +1,87 @@
+# coding=utf-8
+
+"""
+Shells out to get mantrid statistics, which may or may not require sudo access
+
+#### Dependencies
+
+ * /usr/local/bin/mantrid-client
+
+Author: Slawomir Skowron <szibis@gmail.com>
+
+"""
+
+import diamond.collector
+import subprocess
+import os
+import string
+from diamond.collector import str_to_bool
+
+
+class MantridCollector(diamond.collector.Collector):
+
+    def __init__(self, config, handlers):
+        super(MantridCollector, self).__init__(config, handlers)
+
+        self.statcommand = [self.config['bin'], 'stats']
+
+        if str_to_bool(self.config['use_sudo']):
+            self.statcommand.insert(0, self.config['sudo_cmd'])
+
+    def get_default_config_help(self):
+        config_help = super(MantridCollector, self).get_default_config_help()
+        config_help.update({
+            'bin': 'Path to mantrid-client binary',
+            'use_sudo': 'Use sudo?',
+            'sudo_cmd': 'Path to sudo',
+        })
+        return config_help
+
+    def get_default_config(self):
+        """
+        Returns the default collector settings
+        """
+        config = super(MantridCollector, self).get_default_config()
+        config.update({
+            'bin':              '/usr/local/bin/mantrid-client',
+            'use_sudo':         False,
+            'sudo_cmd':         '/usr/bin/sudo',
+            'path':             'mantrid'
+        })
+        return config
+
+    def collect(self):
+        if not os.access(self.config['bin'], os.X_OK):
+            self.log.error("%s is not executable", self.config['bin'])
+            return False
+
+        if (str_to_bool(self.config['use_sudo'])
+            and not os.access(self.config['sudo_cmd'], os.X_OK)):
+
+            self.log.error("%s is not executable", self.config['sudo_cmd'])
+            return False
+
+        p = subprocess.Popen(self.statcommand,
+                             stdout=subprocess.PIPE).communicate()[0][:-1]
+
+        columns = {
+            'open': 1,
+            'completed': 2,
+            'inbytes': 3,
+            'outbytes': 4,
+        }
+
+        for i, line in enumerate(p.split("\n")):
+            if i < 1:
+                continue
+            row = line.split()
+
+            host = string.replace(row[0], ".", "_")
+            host_under = string.replace(host, "-", "_")
+            external = host_under
+
+            for metric, column in columns.iteritems():
+                metric_name = ".".join([external, metric])
+                metric_value = row[column]
+
+                self.publish(metric_name, metric_value)
